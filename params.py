84bf7de7fdcf93a2cb09aed66f3a909662c5420c
Kevin U. Antela <145849005+M1t3vK3y@users.noreply.github.com>
2025-01-31 10:30:41 +0100

    Update params.py

--- params.py
+++ params.py
@@ -9,7 +9,8 @@ urls = [
     (st.secrets["URLS"]["URL1"],st.secrets["KEYS"]["KEY1"],st.secrets["NAMES"]["NAME1"]),
     (st.secrets["URLS"]["URL2"],st.secrets["KEYS"]["KEY2"],st.secrets["NAMES"]["NAME2"]),
     (st.secrets["URLS"]["URL3"],st.secrets["KEYS"]["KEY3"],st.secrets["NAMES"]["NAME3"]),
-    (st.secrets["URLS"]["URL4"],st.secrets["KEYS"]["KEY4"],st.secrets["NAMES"]["NAME4"])
+    (st.secrets["URLS"]["URL4"],st.secrets["KEYS"]["KEY4"],st.secrets["NAMES"]["NAME4"]),
+    (st.secrets["URLS"]["URL5"],st.secrets["KEYS"]["KEY5"],st.secrets["NAMES"]["NAME5"])
 ]
 # Store visibility checkboxes for each labeler
 labelers_visibility = {}
